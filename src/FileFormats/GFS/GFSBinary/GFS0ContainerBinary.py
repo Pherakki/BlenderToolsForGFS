2bd949b0f2358b6d13a76c6db23713741bde968c
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-12-01 23:28:20 +0000

    DEL: Temporarily remove animation support until model loads are OK

--- src/FileFormats/GFS/GFSBinary/GFS0ContainerBinary.py
+++ src/FileFormats/GFS/GFSBinary/GFS0ContainerBinary.py
@@ -5,6 +5,9 @@ from .ModelBinary import ModelBinary
 from .TextureBinary import TextureBinary
 from .AnimationBinary import AnimationDataBinary
 
+class HasAnimationsError(Exception):
+    pass
+
 
 class GFS0ContainerBinary(Serializable):
     SIZE = 0x0C
@@ -13,7 +16,7 @@ class GFS0ContainerBinary(Serializable):
         super().__init__()
         self.context.endianness = endianness
         
-        self.version = 0x01105100
+        self.version = None
         self.type = None
         self.size = None
         self.padding_0x0C = 0
@@ -27,7 +30,13 @@ class GFS0ContainerBinary(Serializable):
         self.version      = rw.rw_uint32(self.version)
         self.type         = rw.rw_uint32(self.type)
         self.size         = rw.rw_uint32(self.size)
-        rw.assert_equal(self.version in [0x01105070, 0x01105100], True)
+        #print(f"0x{self.version:0>8x}")
+        # Need to be extremely careful here...
+        # This is getting dangerous - need to really make sure the version differences
+        # are accounted for before going further
+        # 0x01105080 has a missing element in Materials?
+        # Everything else above 0x1105000 appears to be stable
+        rw.assert_equal(self.version in [0x01105100], True)
         
         # This is a really ugly bit of code that is difficult to read.
         # It's an extremely simple switch, nothing logically complicated...
@@ -54,9 +63,10 @@ class GFS0ContainerBinary(Serializable):
             self.count = rw.rw_uint32(self.count)
             self.data = rw.rw_obj_array(self.data, TextureBinary, self.count)
         elif self.type == 0x000100FD: # Animations
-            self.padding_0x0C = rw.rw_uint32(self.padding_0x0C)
-            self.data = AnimationDataBinary()
-            rw.rw_obj(self.data)
+            raise HasAnimationsError
+        #     self.padding_0x0C = rw.rw_uint32(self.padding_0x0C)
+        #     self.data = AnimationDataBinary()
+        #     rw.rw_obj(self.data)
         else:
             raise NotImplementedError(f"Unrecognised GFS Container Type: '{safe_format(self.type, hex32_format)}'")
             
