26385250c7177d00da64e33e427964df47491afc
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-12-04 15:27:33 +0000

    FIX: End-of-file test

--- src/FileFormats/GFS/GFSBinary/__init__.py
+++ src/FileFormats/GFS/GFSBinary/__init__.py
@@ -32,7 +32,7 @@ class GFS0Binary(Serializable):
                     rw.assert_equal(ctr.size, rw.tell() - start_offset)
                 
                 # Check if final container
-                if ctr.type == 0 or ctr.type == 0x000100FD:
+                if rw.peek_bytestring(1) == b'':
                     finished = True
                     
             # Check there's no more data
