370c5f5a92c250fd31f5af3088d20d8f460884a6
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-12-27 22:55:54 +0000

    FIX: BitVector class definition

--- src/FileFormats/GFS/GFSBinary/CommonStructures.py
+++ src/FileFormats/GFS/GFSBinary/CommonStructures.py
@@ -146,20 +146,27 @@ class BitVector(Serializable):
         self._value = rw.rw_uint32(self._value)
         
     def set_bit(self, bit, value):
+        value = bool(value)
         self._value &= ~(1 << bit)
-        self._value |= (1 << bit)
+        self._value |= (value << bit)
     
     def get_bit(self, bit):
-        return self._value & (1 << bit)
+        return (self._value & (1 << bit)) > 0
     
     def __and__(self, value):
         return self._value & value
     
-    def __iand(self, value):
+    def __iand__(self, value):
         self._value &= value
+        return self
         
     def __or__(self, value):
         return self._value | value
     
-    def __ior(self, value):
+    def __ior__(self, value):
         self._value |= value
+        return self
+
+    @staticmethod
+    def DEF_FLAG(bit):
+        return property(lambda self: self.get_bit(bit), lambda self, x: self.set_bit(bit, x))
