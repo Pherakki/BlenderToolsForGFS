244b62d11f4cfc7ae01e7caabb9c5b43d2fc5334
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-01-02 03:11:03 +0000

    FIX: Improved roll matrix calculation

--- src/BlenderIO/Import.py
+++ src/BlenderIO/Import.py
@@ -68,30 +68,66 @@ class TemporaryFile:
     def write(self, data):
         self.file_handle.write(data)
 
+
 def vec_roll_to_mat3(vec, roll):
     """
-    https://blender.stackexchange.com/a/38337
+    Code from
+    https://blender.stackexchange.com/a/90240
+    with minor edits:
+        - Removed 'mathutils' prefix from Matrix classes
+        - Fixed invalid operation in penultimate line: * -> @
     """
-    target = Vector((0, 0.1, 0))
-    nor = vec.normalized()
-    axis = target.cross(nor)
-    if axis.dot(axis) > 10**-10:
-        axis.normalize()
-        theta = target.angle(nor)
-        bMatrix = Matrix.Rotation(theta, 3, axis)
-    else:
-        updown = 1 if target.dot(nor) > 0 else -1
-        bMatrix = Matrix.Scale(updown, 3)
-        bMatrix[2][2] = 1.0
+    #port of the updated C function from armature.c
+    #https://developer.blender.org/T39470
+    #note that C accesses columns first, so all matrix indices are swapped compared to the C version
 
+    nor = vec.normalized()
+    THETA_THRESHOLD_NEGY = 1.0e-9
+    THETA_THRESHOLD_NEGY_CLOSE = 1.0e-5
+
+    #create a 3x3 matrix
+    bMatrix = Matrix().to_3x3()
+
+    theta = 1.0 + nor[1];
+
+    if (theta > THETA_THRESHOLD_NEGY_CLOSE) or ((nor[0] or nor[2]) and theta > THETA_THRESHOLD_NEGY):
+
+        bMatrix[1][0] = -nor[0];
+        bMatrix[0][1] = nor[0];
+        bMatrix[1][1] = nor[1];
+        bMatrix[2][1] = nor[2];
+        bMatrix[1][2] = -nor[2];
+        if theta > THETA_THRESHOLD_NEGY_CLOSE:
+            #If nor is far enough from -Y, apply the general case.
+            bMatrix[0][0] = 1 - nor[0] * nor[0] / theta;
+            bMatrix[2][2] = 1 - nor[2] * nor[2] / theta;
+            bMatrix[0][2] = bMatrix[2][0] = -nor[0] * nor[2] / theta;
+
+        else:
+            #If nor is too close to -Y, apply the special case.
+            theta = nor[0] * nor[0] + nor[2] * nor[2];
+            bMatrix[0][0] = (nor[0] + nor[2]) * (nor[0] - nor[2]) / -theta;
+            bMatrix[2][2] = -bMatrix[0][0];
+            bMatrix[0][2] = bMatrix[2][0] = 2.0 * nor[0] * nor[2] / theta;
+
+    else:
+        #If nor is -Y, simple symmetry by Z axis.
+        bMatrix = Matrix().to_3x3()
+        bMatrix[0][0] = bMatrix[1][1] = -1.0;
+
+    #Make Roll matrix
     rMatrix = Matrix.Rotation(roll, 3, nor)
+
+    #Combine and output result
     mat = rMatrix @ bMatrix
     return mat
 
 
 def mat3_to_vec_roll(mat):
     """
+    Code from
     https://blender.stackexchange.com/a/38337
+    https://blender.stackexchange.com/a/90240
     """
     vec = mat.col[1]
     vecmat = vec_roll_to_mat3(mat.col[1], 0)
