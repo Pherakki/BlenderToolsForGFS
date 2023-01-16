d9a24d23633353c5d2f48b75b102ef3d7a2568b8
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-01-16 19:38:38 +0000

    ADD: Notes on possible values for Camera data

--- src/FileFormats/GFS/SubComponents/CommonStructures/SceneNode/CameraBinary.py
+++ src/FileFormats/GFS/SubComponents/CommonStructures/SceneNode/CameraBinary.py
@@ -19,9 +19,9 @@ class CameraBinary(Serializable):
             f"{self.zNear} {self.zFar} {self.fov} {self.aspect_ratio} {self.unknown_0x50}"
     
     def read_write(self, rw, version):
-        self.view_matrix  = rw.rw_float32s(self.view_matrix, 16)
-        self.zNear        = rw.rw_float32(self.zNear)
-        self.zFar         = rw.rw_float32(self.zFar)
-        self.fov          = rw.rw_float32(self.fov)
-        self.aspect_ratio = rw.rw_float32(self.aspect_ratio)
-        self.unknown_0x50 = rw.rw_float32(self.unknown_0x50)
+        self.view_matrix  = rw.rw_float32s(self.view_matrix, 16) # Always identity
+        self.zNear        = rw.rw_float32(self.zNear) # 1, 10, 100
+        self.zFar         = rw.rw_float32(self.zFar)  # 4000, 20000, 400000, 600000
+        self.fov          = rw.rw_float32(self.fov)   # Degrees, 0 - 90
+        self.aspect_ratio = rw.rw_float32(self.aspect_ratio) # 1, 4/3, 3/2, 16/9
+        self.unknown_0x50 = rw.rw_float32(self.unknown_0x50) # Always 0
