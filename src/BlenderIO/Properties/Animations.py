95d86409a002c17e233bf971a256cbd6ade1d5d1
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-02-16 23:04:15 +0000

    FIX: Animation default values and name declarations

--- src/BlenderIO/Properties/Animations.py
+++ src/BlenderIO/Properties/Animations.py
@@ -54,37 +54,37 @@ class GFSToolsAnimationProperties(bpy.types.PropertyGroup):
 
     
     # Common properties
-    flag_0:  bpy.props.BoolProperty(name="Unknown Flag 0") # Enable node anims?
-    flag_1:  bpy.props.BoolProperty(name="Unknown Flag 1") # Enable material anims?
-    flag_2:  bpy.props.BoolProperty(name="Unknown Flag 2") # Enable camera anims?
-    flag_3:  bpy.props.BoolProperty(name="Unknown Flag 3") # Enable morph anims?
-    flag_4:  bpy.props.BoolProperty(name="Unknown Flag 4") # Enable type 5 anims?
-    flag_5:  bpy.props.BoolProperty(name="Unknown Flag 5 (Unused?)")
-    flag_6:  bpy.props.BoolProperty(name="Unknown Flag 6 (Unused?)")
-    flag_7:  bpy.props.BoolProperty(name="Unknown Flag 7 (Unused?)")
-    flag_8:  bpy.props.BoolProperty(name="Unknown Flag 8 (Unused?)")
-    flag_9:  bpy.props.BoolProperty(name="Unknown Flag 9 (Unused?)")
-    flag_10: bpy.props.BoolProperty(name="Unknown Flag 10 (Unused?)")
-    flag_11: bpy.props.BoolProperty(name="Unknown Flag 11 (Unused?)")
-    flag_12: bpy.props.BoolProperty(name="Unknown Flag 12 (Unused?)")
-    flag_13: bpy.props.BoolProperty(name="Unknown Flag 13 (Unused?)")
-    flag_14: bpy.props.BoolProperty(name="Unknown Flag 14 (Unused?)")
-    flag_15: bpy.props.BoolProperty(name="Unknown Flag 15 (Unused?)")
-    flag_16: bpy.props.BoolProperty(name="Unknown Flag 16 (Unused?)")
-    flag_17: bpy.props.BoolProperty(name="Unknown Flag 17 (Unused?)")
-    flag_18: bpy.props.BoolProperty(name="Unknown Flag 18 (Unused?)")
-    flag_19: bpy.props.BoolProperty(name="Unknown Flag 19 (Unused?)")
-    flag_20: bpy.props.BoolProperty(name="Unknown Flag 20 (Unused?)")
-    flag_21: bpy.props.BoolProperty(name="Unknown Flag 21 (Unused?)")
-    flag_22: bpy.props.BoolProperty(name="Unknown Flag 22 (Unused?)")
-    flag_24: bpy.props.BoolProperty(name="Unknown Flag 24 (Unused?)")
-    flag_26: bpy.props.BoolProperty(name="Unknown Flag 26 (Unused?)")
-    flag_27: bpy.props.BoolProperty(name="Unknown Flag 27 (Unused?)")
+    flag_0:  bpy.props.BoolProperty(name="Unknown Flag 0", default=True) # Enable node anims?
+    flag_1:  bpy.props.BoolProperty(name="Unknown Flag 1", default=False) # Enable material anims?
+    flag_2:  bpy.props.BoolProperty(name="Unknown Flag 2", default=False) # Enable camera anims?
+    flag_3:  bpy.props.BoolProperty(name="Unknown Flag 3", default=False) # Enable morph anims?
+    flag_4:  bpy.props.BoolProperty(name="Unknown Flag 4", default=False) # Enable type 5 anims?
+    flag_5:  bpy.props.BoolProperty(name="Unknown Flag 5 (Unused?)", default=False)
+    flag_6:  bpy.props.BoolProperty(name="Unknown Flag 6 (Unused?)", default=False)
+    flag_7:  bpy.props.BoolProperty(name="Unknown Flag 7 (Unused?)", default=False)
+    flag_8:  bpy.props.BoolProperty(name="Unknown Flag 8 (Unused?)", default=False)
+    flag_9:  bpy.props.BoolProperty(name="Unknown Flag 9 (Unused?)", default=False)
+    flag_10: bpy.props.BoolProperty(name="Unknown Flag 10 (Unused?)", default=False)
+    flag_11: bpy.props.BoolProperty(name="Unknown Flag 11 (Unused?)", default=False)
+    flag_12: bpy.props.BoolProperty(name="Unknown Flag 12 (Unused?)", default=False)
+    flag_13: bpy.props.BoolProperty(name="Unknown Flag 13 (Unused?)", default=False)
+    flag_14: bpy.props.BoolProperty(name="Unknown Flag 14 (Unused?)", default=False)
+    flag_15: bpy.props.BoolProperty(name="Unknown Flag 15 (Unused?)", default=False)
+    flag_16: bpy.props.BoolProperty(name="Unknown Flag 16 (Unused?)", default=False)
+    flag_17: bpy.props.BoolProperty(name="Unknown Flag 17 (Unused?)", default=False)
+    flag_18: bpy.props.BoolProperty(name="Unknown Flag 18 (Unused?)", default=False)
+    flag_19: bpy.props.BoolProperty(name="Unknown Flag 19 (Unused?)", default=False)
+    flag_20: bpy.props.BoolProperty(name="Unknown Flag 20 (Unused?)", default=False)
+    flag_21: bpy.props.BoolProperty(name="Unknown Flag 21 (Unused?)", default=False)
+    flag_22: bpy.props.BoolProperty(name="Unknown Flag 22 (Unused?)", default=False)
+    flag_24: bpy.props.BoolProperty(name="Unknown Flag 24 (Unused?)", default=False)
+    flag_26: bpy.props.BoolProperty(name="Unknown Flag 26 (Unused?)", default=False)
+    flag_27: bpy.props.BoolProperty(name="Unknown Flag 27 (Unused?)", default=False)
     
     unimported_tracks: bpy.props.StringProperty(name="HiddenUnimportedTracks", default="", options={"HIDDEN"})
     
     # Only for Normal animations
-    has_lookat_anims:    bpy.props.BoolProperty("LookAt Anims")
+    has_lookat_anims:    bpy.props.BoolProperty(name="LookAt Anims")
     lookat_right:        bpy.props.EnumProperty(name="LookAt Right", items=find_lookats)
     lookat_left:         bpy.props.EnumProperty(name="LookAt Left",  items=find_lookats)
     lookat_up:           bpy.props.EnumProperty(name="LookAt Up",    items=find_lookats)
@@ -95,7 +95,7 @@ class GFSToolsAnimationProperties(bpy.types.PropertyGroup):
     lookat_down_factor:  bpy.props.FloatProperty(name="LookAt Down Factor")
     
     # Only for Blend and LookAt animations
-    has_scale_action:   bpy.props.BoolProperty("Has Scale Channel")
+    has_scale_action:   bpy.props.BoolProperty(name="Has Scale Channel")
     blend_scale_action: bpy.props.EnumProperty(name="Scale Channel", items=find_blendscales)
 
     properties:          bpy.props.CollectionProperty(name="Properties", type=GFSToolsGenericProperty)
