8aac6d004e5bd5956aa70dac92ca026f82243fb1
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-02-17 23:04:04 +0000

    ADD: Define Warning classes

--- src/BlenderIO/WarningSystem/Warning.py
+++ src/BlenderIO/WarningSystem/Warning.py
@@ -1,2 +1,14 @@
-class PluginWarning:
+class ReportableWarning:
     pass
+
+class ReportableError:
+    __slots__ = ("msg",)
+    
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def showErrorData(self):
+        pass
+    
+    def hideErrorData(self):
+        pass
