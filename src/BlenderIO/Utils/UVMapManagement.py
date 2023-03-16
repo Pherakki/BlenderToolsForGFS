eb0e035e753e725f9bf4f2abd97fd068834ddaf3
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-03-16 23:03:51 +0000

    REF: Remove case-insensitivity in UV and Color map names

--- src/BlenderIO/Utils/UVMapManagement.py
+++ src/BlenderIO/Utils/UVMapManagement.py
@@ -2,7 +2,7 @@ def make_uv_map_name(idx):
     return f"UV{idx}"
 
 def is_valid_uv_map(name):
-    return (name[:2].upper() == "UV") and (name[2:].isdigit())
+    return (name[:2] == "UV") and (name[2:].isdigit())
 
 def get_uv_idx_from_name(name):
     return int(name[2:])
@@ -12,7 +12,7 @@ def make_color_map_name(idx):
     return f"Map{idx}"
 
 def is_valid_color_map(name):
-    return (name[:3].upper() == "MAP") and (name[3:].isdigit())
+    return (name[:3] == "Map") and (name[3:].isdigit())
 
 def get_color_idx_from_name(name):
     return int(name[3:])
