8bdc8e3c0a0a4f6e2217f734975f7d54cf2f57c8
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-03-17 22:53:39 +0000

    FIX: Argument passing to import_materials

--- src/BlenderIO/Import/Operator.py
+++ src/BlenderIO/Import/Operator.py
@@ -80,7 +80,7 @@ class ImportGFS(bpy.types.Operator, ImportHelper):
 
         # Now import file data to Blender
         textures  = import_textures(gfs)
-        materials = import_materials(gfs, textures)
+        materials = import_materials(gfs, textures, errorlog)
         armature, gfs_to_bpy_bone_map, mesh_node_map = import_model(gfs, os.path.split(filepath)[1].split('.')[0], materials, errorlog, self.merge_vertices)
         
         create_rest_pose(gfs, armature, gfs_to_bpy_bone_map)
