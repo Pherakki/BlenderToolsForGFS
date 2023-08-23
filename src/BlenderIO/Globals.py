5311fb894d16f04ff4861f725ee6bb85afd2d14d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-08-23 22:59:55 +0100

    ADD: Initial ModelTransforms logic

--- src/BlenderIO/Globals.py
+++ src/BlenderIO/Globals.py
@@ -1,1 +1,16 @@
+import bpy
+from mathutils import Matrix
+
 from . import modelUtilsTest as blenderModelSupportUtils
+bm = blenderModelSupportUtils
+
+
+GFS_MODEL_TRANSFORMS = bm.ModelTransforms()
+GFS_MODEL_TRANSFORMS.world_axis_rotation = Matrix([[ 1.,  0.,  0.,  0.],
+                                                   [ 0.,  0., -1.,  0.],
+                                                   [ 0.,  1.,  0.,  0.],
+                                                   [ 0.,  0.,  0.,  1.]])
+GFS_MODEL_TRANSFORMS.bone_axis_permutation = Matrix([[ 0.,  1.,  0.,  0.],
+                                                     [-1.,  0.,  0.,  0.],
+                                                     [ 0.,  0.,  1.,  0.],
+                                                     [ 0.,  0.,  0.,  1.]])
