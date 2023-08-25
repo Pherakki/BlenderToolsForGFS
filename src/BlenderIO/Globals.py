1240624c6c73679f0f498524d7adccd601c08165
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-08-25 22:12:40 +0100

    REF: Change bm import to bmu to prevent clash with bmesh

--- src/BlenderIO/Globals.py
+++ src/BlenderIO/Globals.py
@@ -2,8 +2,8 @@ import bpy
 from mathutils import Matrix
 
 from . import modelUtilsTest as blenderModelSupportUtils
-bm = blenderModelSupportUtils
+bmu = blenderModelSupportUtils
 
 
-GFS_MODEL_TRANSFORMS = bm.ModelTransforms(world_axis=['X', 'Z', '-Y'], 
-                                          bone_axis=['-Y', 'X', 'Z'])
+GFS_MODEL_TRANSFORMS = bmu.ModelTransforms(world_axis=['X', 'Z', '-Y'], 
+                                           bone_axis=['-Y', 'X', 'Z'])
