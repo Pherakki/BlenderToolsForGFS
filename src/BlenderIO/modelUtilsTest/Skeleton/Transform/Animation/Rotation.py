5dfff4df48099a8627e102726cf6f6a37e0a3c50
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-09-06 20:16:38 +0100

    FIX: Attribute names

--- src/BlenderIO/modelUtilsTest/Skeleton/Transform/Animation/Rotation.py
+++ src/BlenderIO/modelUtilsTest/Skeleton/Transform/Animation/Rotation.py
@@ -30,19 +30,19 @@ parent_to_bind_rotation = TransformFunc(parent_to_bind_rotation_quaternion,
 # Bind-pose-relative -> Parent-relative
 def bind_to_parent_rotation_quaternion(rotations, bone_transforms, model_transforms):
     bind_pose_rotation        = bone_transforms.rotation.quat
-    inv_bone_axis_permutation = model_transforms.bone_axis_permuation.quat_inv
+    inv_bone_axis_permutation = model_transforms.bone_axis_permutation.quat_inv
     return [bind_pose_rotation @ Quaternion(v) @ inv_bone_axis_permutation for v in rotations]
 
 
 def bind_to_parent_rotation_matrix3x3(rotations, bone_transforms, model_transforms):
     bind_pose_rotation        = bone_transforms.rotation.matrix3x3
-    inv_bone_axis_permutation = model_transforms.bone_axis_permuation.matrix3x3_inv
+    inv_bone_axis_permutation = model_transforms.bone_axis_permutation.matrix3x3_inv
     return [bind_pose_rotation @ v @ inv_bone_axis_permutation for v in rotations]
 
 
 def bind_to_parent_rotation_matrix4x4(rotations, bone_transforms, model_transforms):
     bind_pose_rotation        = bone_transforms.rotation.matrix4x4
-    inv_bone_axis_permutation = model_transforms.bone_axis_permuation.matrix4x4_inv
+    inv_bone_axis_permutation = model_transforms.bone_axis_permutation.matrix4x4_inv
     return [bind_pose_rotation @ v @ inv_bone_axis_permutation for v in rotations]
 
 
