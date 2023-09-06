179d48c200e4cec666792a4cf488ba9bb667603d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-09-06 20:18:17 +0100

    REF: Use external library functions to export animation data

--- src/BlenderIO/Export/ExportAnimations.py
+++ src/BlenderIO/Export/ExportAnimations.py
@@ -1,14 +1,14 @@
 import io
 
-import bpy
-from mathutils import Matrix, Quaternion, Vector
-import numpy as np
-
 from ...serialization.BinaryTargets import Reader
 from ...FileFormats.GFS.SubComponents.Animations import AnimationInterface, AnimationBinary
 from ...FileFormats.GFS.SubComponents.Animations.Binary.AnimationBinary import EPLEntry
-from ..Utils.Maths import convert_rotation_to_quaternion, convert_XDirBone_to_YDirBone, convert_Zup_to_Yup
-from ..Utils.Interpolation import lerp
+
+from ..Globals import GFS_MODEL_TRANSFORMS
+from ..modelUtilsTest.Skeleton.Transform.Animation import bind_to_parent, bind_to_parent_blend
+from ..modelUtilsTest.Skeleton.Transform.Animation.Extract import synchronised_quat_bone_data_from_fcurves
+from ..modelUtilsTest.Skeleton.Transform.Animation.Extract import synchronised_quat_object_transforms_from_fcurves
+from ..modelUtilsTest.Skeleton.Transform.Animation.Extract import extract_fcurves
 
 
 def export_animations(gfs, armature, keep_unused_anims):
@@ -233,87 +233,40 @@ def remap_track_id(track, names, keep_unused_anims):
     
     return True
 
-#############################
-# STUFF THAT SHOULD BE USED #
-#############################
-def get_action_data(action, armature, is_blend):
-    curve_defaults = {'location': [0., 0., 0.],
-                      'rotation_quaternion': [1., 0., 0., 0.],
-                      'scale': [1., 1., 1.],
-                      'rotation_euler': [0, 0, 0]}
-    
-    bone_names = [b.name for b in armature.data.bones]
+
+def get_action_data(action, bpy_armature_obj, is_blend):
+    bone_names = [b.name for b in bpy_armature_obj.data.bones]
     out = []
-    animation_data = {}
-    fcurve_groups, obj_transforms = group_fcurves_by_bone_and_type(action)
-    for bone_name, group in fcurve_groups.items():
-        if bone_name not in armature.pose.bones:
+
+    fcurves = extract_fcurves(action)
+    bone_fcurves = synchronised_quat_bone_data_from_fcurves(fcurves, bpy_armature_obj.pose.bones)
+    obj_fcurves  = synchronised_quat_object_transforms_from_fcurves(fcurves, bpy_armature_obj)
+    
+    # print(fcurve_groups)
+    for bone_name, group in bone_fcurves.items():
+        if bone_name not in bpy_armature_obj.pose.bones:
             continue
-        animation_data[bone_name] = {'rotation_quaternion': {},
-                                     'location': {},
-                                     'scale': {},
-                                     'rotation_euler': {}}
-        extract_clean_animation_data(group, curve_defaults, animation_data[bone_name], armature.pose.bones[bone_name])
-        
-        bpy_bone = armature.data.bones[bone_name]
+
+        bpy_bone = bpy_armature_obj.data.bones[bone_name]
         override_name = bpy_bone.GFSTOOLS_NodeProperties.override_name
         if is_blend:
-            # Extract animation data
-            positions = animation_data[bone_name].get("location", {})
-            rotations = animation_data[bone_name].get("rotation_quaternion", {})
-            scales    = animation_data[bone_name].get("scale", {})
-            
-            # Get the matrices required to convert animations from Blender -> GFS
-            axis_conversion = convert_XDirBone_to_YDirBone(Matrix.Identity(4))
-            if bpy_bone.parent is not None:
-                local_bind_matrix = axis_conversion @ bpy_bone.parent.matrix_local.inverted() @ bpy_bone.matrix_local
-            else:
-                local_bind_matrix = convert_Zup_to_Yup(bpy_bone.matrix_local)
-                
-            # This can DEFINITELY be made more efficient if it's a bottleneck
-            bind_pose_translation, bind_pose_quaternion, _ = local_bind_matrix.decompose()
-            bind_pose_rotation     = bind_pose_quaternion.to_matrix().to_4x4()
-            inv_bind_pose_rotation = bind_pose_rotation.inverted()
-            inv_axis_conversion    = axis_conversion.inverted()
-            
-            g_positions = {k: bind_pose_rotation @ Matrix.Translation(v)                                      @ inv_bind_pose_rotation for k, v in positions.items()}
-            g_rotations = {k: axis_conversion    @ Quaternion([v[3], v[0], v[1], v[2]]).to_matrix().to_4x4()  @ inv_axis_conversion    for k, v in rotations.items()}
-            g_scales    = {k: axis_conversion    @ Matrix.Diagonal([*v, 1.])                                  @ inv_axis_conversion    for k, v in scales.items()   }
-            
-            g_positions = {k: v.decompose()[0]     for k, v in g_positions.items()}
-            g_rotations = {k: q.decompose()[1]     for k, q in g_rotations.items()}
-            g_rotations = {k: [q.x, q.y, q.z, q.w] for k, q in g_rotations.items()}
-            g_scales    = {k: v.decompose()[2]     for k, v in g_scales.items()}
-            
+            positions = bone_fcurves[bone_name].get("location", {})
+            rotations = bone_fcurves[bone_name].get("rotation_quaternion", {})
+            scales    = bone_fcurves[bone_name].get("scale", {})
+
+            g_positions, g_rotations, g_scales = bind_to_parent_blend(bpy_bone, positions.values(), rotations.values(), scales.values(), GFS_MODEL_TRANSFORMS)
+            g_positions = {k: v                    for k, v in zip(positions.keys(), g_positions)}
+            g_rotations = {k: [q.x, q.y, q.z, q.w] for k, q in zip(rotations.keys(), g_rotations)}
+            g_scales    = {k: v                    for k, v in zip(scales.keys(),    g_scales   )}
         else:
-            # Extract animation data
-            positions = animation_data[bone_name].get("location", {})
-            rotations = animation_data[bone_name].get("rotation_quaternion", {})
-            scales    = animation_data[bone_name].get("scale", {})
+            positions = bone_fcurves[bone_name].get("location", {})
+            rotations = bone_fcurves[bone_name].get("rotation_quaternion", {})
+            scales    = bone_fcurves[bone_name].get("scale", {})
             
-            # Get the matrices required to convert animations from Blender -> GFS
-            axis_conversion = convert_XDirBone_to_YDirBone(Matrix.Identity(4))
-            bpy_bone = armature.data.bones[bone_name]
-            if bpy_bone.parent is not None:
-                local_bind_matrix = axis_conversion @ bpy_bone.parent.matrix_local.inverted() @ bpy_bone.matrix_local
-            else:
-                local_bind_matrix = convert_Zup_to_Yup(bpy_bone.matrix_local)
-                
-            # This can DEFINITELY be made more efficient if it's a bottleneck
-            bind_pose_translation, bind_pose_quaternion, _ = local_bind_matrix.decompose()
-            bind_pose_rotation     = bind_pose_quaternion.to_matrix().to_4x4()
-            inv_bind_pose_rotation = bind_pose_rotation.inverted()
-            inv_axis_conversion    = axis_conversion.inverted()
-            bptm = Matrix.Translation(bind_pose_translation)
-            
-            g_positions = {k: (bind_pose_rotation @ Matrix.Translation(Vector(v))                             @ inv_bind_pose_rotation) + bptm for k, v in positions.items()}
-            g_rotations = {k: bind_pose_rotation  @ Quaternion([v[3], v[0], v[1], v[2]]).to_matrix().to_4x4() @ inv_axis_conversion            for k, v in rotations.items()}
-            g_scales    = {k: axis_conversion     @ Matrix.Diagonal([*v, 1.])                                 @ inv_axis_conversion            for k, v in scales.items()   }
-            
-            g_positions = {k: v.decompose()[0]     for k, v in g_positions.items()}
-            g_rotations = {k: q.decompose()[1]     for k, q in g_rotations.items()}
-            g_rotations = {k: [q.x, q.y, q.z, q.w] for k, q in g_rotations.items()}
-            g_scales    = {k: v.decompose()[2]     for k, v in g_scales.items()}
+            g_positions, g_rotations, g_scales = bind_to_parent(bpy_bone, positions.values(), rotations.values(), scales.values(), GFS_MODEL_TRANSFORMS)
+            g_positions = {k: v                    for k, v in zip(positions.keys(), g_positions)}
+            g_rotations = {k: [q.x, q.y, q.z, q.w] for k, q in zip(rotations.keys(), g_rotations)}
+            g_scales    = {k: v                    for k, v in zip(scales.keys(),    g_scales   )}
             
         export_name = override_name if override_name != "" else bone_name
         fps = 30
@@ -323,235 +276,17 @@ def get_action_data(action, armature, is_blend):
                     {(k-1)/fps : v for k, v in g_scales.items()}])
 
     root_animation = None
-    if obj_transforms is not None:
-        root_animation_buffer = {'rotation_quaternion': {},
-                                 'location':            {},
-                                 'scale':               {},
-                                 'rotation_euler':      {}}
-        extract_clean_animation_data(obj_transforms, curve_defaults, root_animation_buffer, armature)
-    
+    if len(obj_fcurves):
         # Extract animation data
-        positions = root_animation_buffer.get("location", {})
-        rotations = root_animation_buffer.get("rotation_quaternion", {})
+        positions = obj_fcurves.get("location", {})
+        rotations = obj_fcurves.get("rotation_quaternion", {})
         rotations = {k: [q.x, q.y, q.z, q.w] for k, q in rotations.items()}
-        scales    = root_animation_buffer.get("scale", {})
+        scales    = obj_fcurves.get("scale", {})
     
         fps = 30
-        root_animation = ([0, armature.data.GFSTOOLS_ModelProperties.root_node_name, 
+        root_animation = ([0, bpy_armature_obj.data.GFSTOOLS_ModelProperties.root_node_name, 
                           {(k-1)/fps : v for k, v in positions.items()}, 
                           {(k-1)/fps : v for k, v in rotations.items()}, 
                           {(k-1)/fps : v for k, v in scales.items()}])
         
     return out, root_animation
-
-
-#####################
-# UTILITY FUNCTIONS #
-#####################
-
-def get_bone_name_from_fcurve(fcurve):
-    return fcurve.data_path.split('[')[1].split(']')[0][1:-1]
-
-
-def get_fcurve_type(fcurve):
-    return fcurve.data_path.split('.')[-1]
-
-
-def create_anim_init_data():
-    return {'rotation_quaternion': [None, None, None, None],
-            'location':            [None, None, None],
-            'scale':               [None, None, None],
-            'rotation_euler':      [None, None, None]}
-
-
-def group_fcurves_by_bone_and_type(action):
-    res = {}
-    possible_transforms = set(create_anim_init_data().keys())
-    obj_transforms = None
-    
-    for fcurve in action.fcurves:
-        # Bone transform
-        if fcurve.data_path[:10] == 'pose.bones':
-            bone_name = get_bone_name_from_fcurve(fcurve)
-            if bone_name not in res: res[bone_name] = create_anim_init_data()
-            curve_type = get_fcurve_type(fcurve)
-            edit_transforms = res[bone_name]
-        # Object transforms
-        elif fcurve.data_path in possible_transforms:
-            if obj_transforms is None: obj_transforms = create_anim_init_data()
-            curve_type = fcurve.data_path
-            edit_transforms = obj_transforms
-        else:
-            continue
-        
-        array_index = fcurve.array_index
-        edit_transforms[curve_type][array_index] = fcurve
-            
-    return res, obj_transforms
-
-
-def get_used_animation_elements_in_group(group):
-    """
-    Summary
-    -------
-    Takes a list of f-curves and assigns the keyframe point co-ordinates in each f-curve to the appropriate transform
-    and array index of a returned dictionary.
-
-    The animation export module should probably be refactored so that the groups that get passed into this function
-    are either locations, rotations, or scales, so that all three are not handled simultaneously, but rather by three
-    separate function calls.
-
-    Parameters
-    ----------
-    :parameters:
-    group -- A list of Blender f-curve objects.
-
-    Returns
-    -------
-    :returns:
-    A two-element tuple:
-    - The first element is a dictionary in the shape {'location': bool, 'rotation_quaternion': bool, 'scale': bool} that
-      states whether any of the input f-curves are of any of those types
-    - The second element is a dictionary in the shape
-                {'location': {0: {}, 1: {}, 2: {}},
-                 'rotation_quaternion': {0: {}, 1: {}, 2: {}, 3: {}},
-                 'scale': {0: {}, 1: {}, 2: {}}},
-      where the integers are the array indices of the appropriate f-curves. Each array index is also given a dictionary
-      as above, which contains the frame index and the f-curve value as key-value pairs.
-    """
-    elements_used = {'location': False,
-                     'rotation_quaternion': False,
-                     'scale': False,
-                     'rotation_euler': False}
-
-    bone_data = {'rotation_quaternion': [{}, {}, {}, {}],
-                 'location':            [{}, {}, {}],
-                 'scale':               [{}, {}, {}],
-                 'rotation_euler':      [{}, {}, {}]}
-    for curve_type in group:
-        for curve_idx, f_curve in enumerate(group[curve_type]):
-            if f_curve is None:
-                continue
-            elements_used[curve_type] = True
-            bone_data[curve_type][curve_idx] = {k: v for k, v in [kfp.co for kfp in f_curve.keyframe_points]}
-
-    return elements_used, bone_data
-
-
-def get_all_required_frames(curve_data):
-    """
-    Returns all keys in a list of dictionaries as a sorted list of rounded integers plus the rounded-up final key,
-    assuming all keys are floating-point values.
-    """
-    res = set()
-    for dct in curve_data:
-        iter_keys = tuple(dct.keys())
-        for key in iter_keys:
-            res.add(key)
-    return sorted(list(res))
-
-
-def interpolate_missing_frame_elements(curve_data, default_values, interpolation_function):
-    """
-    GFS requires animations to be stored as whole quaternions, locations, and scales.
-    This function ensures that every passed f-curve has a value at every frame referenced by all f-curves - e.g. if
-    a location has values at frame 30 on its X f-curve but not on its Y and Z f-curves, the Y and Z values at frame 30
-    will be interpolated from the nearest frames on the Y and Z f-curves respectively and stored in the result.
-    """
-    # First get every frame required by the vector and which will be passed on to GFS
-    all_frame_idxs = get_all_required_frames(curve_data)
-    for (component_idx, framedata), default_value in zip(enumerate(curve_data), default_values):
-        # Get all the frames at which the curve has data
-        component_frame_idxs = list(framedata.keys())
-        # Produce a function that will return the value for the frame, based on how many frames are available
-        interp_method = produce_interpolation_method(component_frame_idxs, framedata, default_value, interpolation_function)
-        new_framedata = {}
-        # Generate the GFS-compatible data
-        for frame_idx in all_frame_idxs:
-            if frame_idx not in component_frame_idxs:
-                new_framedata[frame_idx] = interp_method(frame_idx)
-            else:
-                new_framedata[frame_idx] = framedata[frame_idx]
-
-        curve_data[component_idx] = new_framedata
-
-    return curve_data
-
-
-def zip_vector_elements(curve_data):
-    """
-    Takes n dictionaries in a list, with each dictionary containing the frame indices (as keys) and values (as values)
-    of a single component of a vector. All dictionaries must have exactly the same keys (frame indices).
-    Returns a single dictionary with the frame indices as keys, and the vector components for that frame stored in a
-    list as the value for that key.
-    """
-    new_curve_data = {}
-    for frame_idxs in zip(*[list(e.keys()) for e in curve_data]):
-        for frame_idx in frame_idxs:
-            assert frame_idx == frame_idxs[0]
-        frame_idx = frame_idxs[0]
-        new_curve_data[frame_idx] = [e[frame_idx] for e in curve_data]
-    return new_curve_data
-
-
-def interpolate_keyframe(frame_idxs, frame_values, idx, interpolation_function):
-    smaller_elements = [fidx for fidx in frame_idxs if idx >= fidx]
-    next_smallest_frame = max(smaller_elements) if len(smaller_elements) else frame_idxs[0]
-    larger_elements = [fidx for fidx in frame_idxs if idx <= fidx]
-    next_largest_frame = min(larger_elements) if len(larger_elements) else frame_idxs[-1]
-
-    if next_largest_frame == next_smallest_frame:
-        t = 0  # Totally arbitrary, since the interpolation will be between two identical values
-    else:
-        t = (idx - next_smallest_frame) / (next_largest_frame - next_smallest_frame)
-
-    # Should change lerp to the proper interpolation method
-    min_value = frame_values[next_smallest_frame]
-    max_value = frame_values[next_largest_frame]
-
-    return interpolation_function(np.array(min_value), np.array(max_value), t)
-
-
-def produce_interpolation_method(frame_idxs, frame_values, default_value, interpolation_function):
-    """
-    Returns an interpolation function dependant on the number of passed frames.
-    """
-    if len(frame_idxs) == 0:
-        def interp_method(input_frame_idx):
-            return default_value
-    elif len(frame_idxs) == 1:
-        value = frame_values[frame_idxs[0]]
-
-        def interp_method(input_frame_idx):
-            return value
-    else:
-        def interp_method(input_frame_idx):
-            return interpolate_keyframe(frame_idxs, frame_values, input_frame_idx, interpolation_function)
-
-    return interp_method
-
-
-def extract_clean_animation_data(group, curve_defaults, out_buffer, pose_object):
-    # Get whether any of the locations, rotations, and scales are animated; plus the f-curves for those
-    # that are
-    elements_used, bone_data = get_used_animation_elements_in_group(group)
-    # For each set that is animated, interpolate missing keyframes for each component of the relevant vector
-    # on each keyframe where at least one element is used
-    for curve_type, isUsed in elements_used.items():
-        if isUsed:
-            curve_data = interpolate_missing_frame_elements(bone_data[curve_type], curve_defaults[curve_type], lerp)
-            zipped_data = zip_vector_elements(curve_data)
-            out_buffer[curve_type] = zipped_data
-       
-    rotation_mode = pose_object.rotation_mode
-    if rotation_mode != "QUATERNION":
-        out_buffer["rotation_quaternion"] = {
-            k: convert_rotation_to_quaternion(None, v, rotation_mode) 
-            for k, v in out_buffer["rotation_euler"].items()
-        }
-    
-    elif "rotation_quaternion" in out_buffer:
-        out_buffer["rotation_quaternion"] = {
-            k: [v[1], v[2], v[3], v[0]] 
-            for k, v in out_buffer["rotation_quaternion"].items()
-        }
