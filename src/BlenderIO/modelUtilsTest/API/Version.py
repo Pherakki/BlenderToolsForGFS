dd657a2ecb0fd052ef52eb22ee6a03385af20f39
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-10-13 21:21:48 +0100

    FIX: Version checker

--- src/BlenderIO/modelUtilsTest/API/Version.py
+++ src/BlenderIO/modelUtilsTest/API/Version.py
@@ -2,4 +2,4 @@ import bpy
 
 
 def bpy_at_least(major, minor, patch):
-    return all([v1 >= v2 for v1, v2 in zip((major, minor, patch), bpy.app.version)])
+    return (major, minor, patch) <= bpy.app.version
