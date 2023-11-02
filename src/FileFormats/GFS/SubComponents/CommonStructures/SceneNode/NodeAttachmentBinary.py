623cbdd81094d63ca7a4b0c345dd5740ae37b275
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-11-02 20:39:24 +0000

    REF: Make attachment type lookup more readable

--- src/FileFormats/GFS/SubComponents/CommonStructures/SceneNode/NodeAttachmentBinary.py
+++ src/FileFormats/GFS/SubComponents/CommonStructures/SceneNode/NodeAttachmentBinary.py
@@ -22,20 +22,13 @@ class NodeAttachmentBinary(Serializable):
         self.type = rw.rw_uint32(self.type)
         
         if rw.mode() == "read":
-            if self.type == 4:
-                dtype = MeshBinary
-            elif self.type == 5:
-                dtype = CameraBinary 
-            elif self.type == 6:
-                dtype = LightBinary
-            elif self.type == 7:
-                dtype = EPLBinary
-            elif self.type == 8:
-                dtype = EPLLeafBinary
-            elif self.type == 9:
-                dtype = MorphBinary
-            else:
-                raise NotImplementedError(f"Unrecognised NodeAttachment type: '{self.type}'")
+            if   self.type == 4: dtype = MeshBinary
+            elif self.type == 5: dtype = CameraBinary 
+            elif self.type == 6: dtype = LightBinary
+            elif self.type == 7: dtype = EPLBinary
+            elif self.type == 8: dtype = EPLLeafBinary
+            elif self.type == 9: dtype = MorphBinary
+            else: raise NotImplementedError(f"Unrecognised NodeAttachment type: '{self.type}'")
             self.data = dtype()
             
         rw.rw_obj(self.data, version)
