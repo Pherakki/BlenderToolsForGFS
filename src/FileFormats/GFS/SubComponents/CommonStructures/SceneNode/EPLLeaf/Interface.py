72a5b14cad065c951e7f99ff4ccb589bdfc1389c
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-11-06 21:51:12 +0000

    FIX: Return from EPLLeafInterface

--- src/FileFormats/GFS/SubComponents/CommonStructures/SceneNode/EPLLeaf/Interface.py
+++ src/FileFormats/GFS/SubComponents/CommonStructures/SceneNode/EPLLeaf/Interface.py
@@ -7,9 +7,11 @@ class EPLLeafInterface:
         self.binary = None
         
     @classmethod
-    def from_binary(self, node_idx, binary):
-        self.node = node_idx
-        self.binary = binary
+    def from_binary(cls, node_idx, binary):
+        instance = cls()
+        instance.node = node_idx
+        instance.binary = binary
+        return instance
     
     def to_binary(self):
         return self.binary
