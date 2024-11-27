fa73934b1322ce54a35ee1ad099428a981859f67
Pherakki <71677509+Pherakki@users.noreply.github.com>
2024-11-27 00:22:16 +0000

    WIPFIX: Remove Serializable...

--- src/serialization/Serializable.py
+++
@@ -1,9 +0,0 @@
-from .parsers import GFSReader, GFSWriter, GFSValidator
-from ...external.exbip.serializables.traits import ReadableTrait
-from ...external.exbip.serializables.traits import WriteableTrait
-from ...external.exbip.serializables.traits import ValidatableTrait
-
-class GFSSerializable(ReadableTrait(GFSReader),
-                      WriteableTrait(GFSWriter),
-                      ValidatableTrait(GFSValidator)):
-    pass
