415eea1bc72817c7d44e13164c5235eb10c43f66
Pherakki <71677509+Pherakki@users.noreply.github.com>
2025-01-03 01:57:58 +0000

    REF: Bump version

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ bl_info = {
         "name": "GFS Import/Export (.GMD/.GAP/.GFS)",
         "description": "Imports GFS files.",
         "author": "Pherakki",
-        "version": (0, 3, 0),
+        "version": (0, 3, 1),
         "blender": (2, 81, 0),
         "location": "File > Import, File > Export",
         "warning": "",
